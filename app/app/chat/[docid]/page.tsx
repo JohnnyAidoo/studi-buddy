96f5e72935dad73cc3332a9bee313d13ff07bf8f
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2024-06-23 12:43:53 +0100

    commit

--- app/app/chat/[docid]/page.tsx
+++ app/app/chat/[docid]/page.tsx
@@ -33,7 +33,7 @@ export default function DocChatPage({ params }: { params: { docId: string } }) {
 
   const headers = {
     // "x-api-key": "ask_c816c4e5ddd8c05c53830d3f8bad3d7d","ask_4b765fb8f67d6395b93e13c9339bbfaa"
-    "x-api-key": "ask_c816c4e5ddd8c05c53830d3f8bad3d7d",
+    "x-api-key": process.env.ASK_PDF_API_KEY,
   };
 
   //functions
