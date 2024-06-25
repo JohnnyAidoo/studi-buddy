fa912fdc56e6800c648482a6d3ab2d51d82d4856
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2024-06-25 15:12:04 +0100

    commit

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,12 +1,18 @@
 import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
+import logoImg from "@/public/icon.png";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       <header className="flex justify-between px-10 py-3 shadow-sm  w-full top-0 z-10">
-        <Link href="/app">
-          <h1>Docu-Chat</h1>
+        <Link
+          href="/app"
+          className="mr-4 cursor-pointer py-1.5 font-extrabold text-2sxl flex items-center gap-2 justify-center"
+        >
+          <Image src={logoImg} alt={"logo"} width={50} height={50} />{" "}
+          <span>DOCU-CHAT</span>
         </Link>
 
         <UserButton />
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -11,15 +11,6 @@ import Image from "next/image";
 import logoImg from "@/public/icon.png";
 
 export function Header() {
-  const [openNav, setOpenNav] = React.useState(false);
-
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false)
-    );
-  }, []);
-
   const navList = (
     <ul className="mt-2 mb-4  flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Link href="#features" className="hover:underline">
@@ -35,20 +26,21 @@ export function Header() {
   );
 
   return (
-    <div className=" max-h-[768px] w-[calc(100%+20px)] overflow-scroll bg-primary ">
+    <div className=" max-h-[768px] w-[calc(100%+20px)] overflow-scroll bg-primary  ">
       <Navbar
-        className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4"
+        className="sticky top-0 z-10 h-max max-w-full rounded-none py-2 lg:px-8 lg:py-4"
         placeholder={undefined}
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
       >
         <div className="flex items-center justify-between text-blue-gray-900">
-          <a
-            href="/"
-            className="mr-4 cursor-pointer py-1.5 font-extrabold text-3xl"
+          <Link
+            href="/app"
+            className="mr-4 cursor-pointer py-1.5 font-extrabold text-2sxl flex items-center gap-2 justify-center"
           >
-            <Image src={logoImg} alt={"logo"} width={50} height={50} />
-          </a>
+            <Image src={logoImg} alt={"logo"} width={50} height={50} />{" "}
+            <span>DOCU-CHAT</span>
+          </Link>
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
             <div className="flex items-center gap-x-1">
@@ -74,75 +66,8 @@ export function Header() {
                 </Button>
               </a>
             </div>
-            <IconButton
-              placeholder={undefined}
-              onPointerEnterCapture={undefined}
-              onPointerLeaveCapture={undefined}
-              variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden "
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
-            >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </IconButton>
           </div>
         </div>
-        <MobileNav open={openNav}>
-          {navList}
-          <div className="flex items-center gap-x-1">
-            <Button
-              placeholder={undefined}
-              onPointerEnterCapture={undefined}
-              onPointerLeaveCapture={undefined}
-              fullWidth
-              variant="text"
-              size="sm"
-              className=""
-            >
-              <span>Log In</span>
-            </Button>
-            <Button
-              placeholder={undefined}
-              onPointerEnterCapture={undefined}
-              onPointerLeaveCapture={undefined}
-              fullWidth
-              variant="gradient"
-              size="sm"
-              className=""
-            >
-              <span>Sign in</span>
-            </Button>
-          </div>
-        </MobileNav>
       </Navbar>
     </div>
   );
