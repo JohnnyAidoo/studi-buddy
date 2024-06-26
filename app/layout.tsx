e04e7f0234a6a93178f66a53bc421824c3a446ed
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2024-06-26 16:57:10 +0100

    added og meta detail

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
+import MainURL from "./components/url";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -28,6 +29,23 @@ export default function RootLayout({
     <>
       <ClerkProvider>
         <html lang="en">
+          <head>
+            <meta property="og:title" content="Docu-Chat" />
+            <meta
+              property="og:description"
+              content="Experience seamless communication with Docu-Chat. Chat directly with your documents and streamline your workflow."
+            />
+            <meta property="og:image" content="../public/banner.png" />
+            <meta property="og:url" content={MainURL} />
+            <meta name="twitter:card" content="Docu-Chat" />
+            <meta name="twitter:title" content="Docu-Chat" />
+            <meta
+              name="twitter:description"
+              content="Chat with your documents effortlessly. Enhance productivity and streamline your workflow with Docu-Chat."
+            />
+            <meta name="twitter:image" content="../public/banner.png" />
+            <link rel="icon" href="../public/icon.png" />
+          </head>
           <body
             className={`${poppins.variable} bg-primary `}
             style={{ overflowX: "hidden" }}
